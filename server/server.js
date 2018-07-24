871602ada8cad1dc6e8abf77382fe1e7ab2f47f6
gpyshenko <gpyshenko@gmail.com>
2018-07-24 18:59:35 +0500

    CORS - how do it

--- server/server.js
+++ server/server.js
@@ -11,6 +11,13 @@ var app = express();
 
 app.use(bodyParser.json());
 
+// - Для CORS
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
+
 app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text,
