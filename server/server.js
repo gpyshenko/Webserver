104a4bce5877134021d8470308b6e75546f01159
gpyshenko <gpyshenko@gmail.com>
2018-10-18 22:42:41 +0500

    Update

--- server/server.js
+++ server/server.js
@@ -38,32 +38,32 @@ app.get('/contacts', (req,res) => {
     res.render('contacts.pug', {
         pageTitle: 'Контакты'
     })
-})
+});
 
 app.get('/contacts/:id', (req, res) => {
     res.render('contacts.pug', {
         pageTitle: 'Контакты',
         office: req.params.id
     })
-})
+});
 
 app.post('/contacts/submit', (req, res) => {
     var id = req.body.id;
     res.redirect('/contacts/' + id)
-})
+});
 
 app.use(function(req,res,next) {
     res.status(404);
     res.render('404.pug', {
         pageTitle: 'Страница 404'
     })
-})
+});
 
 app.use(function (err,req,res,next) {
     console.log(err.stack);
     res.status(500);
     res.render('500.pug')
-})
+});
 
 app.listen(3000, () => {
     console.log(`Started on port 3000`);
